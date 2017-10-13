dc8374cfd951e7068227ddaef3b72a860337bdc5
Lorenzo Gandino <lorenzoga94@gmail.com>
2017-10-13 13:12:14 +0200

    Aggiute informazioni più dettagliate al main, aggiunto file di test
    Aggiunto il resoconto dei dati inseriti in input mostrando quali hanno
    causato l'errore.
    
    Il file di test permette di testare l'esercizio esp_1.

--- src/esercizi/BaseEsercizio.java
+++ src/esercizi/BaseEsercizio.java
@@ -1,25 +1,47 @@
 package esercizi;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 import it.gandinolorenzo.lft.Automa;
 
 public abstract class BaseEsercizio {
+	
+	private class ErrorBox{
+		int riga;
+		String stringa;
+		public ErrorBox(int riga, String stringa) {
+			super();
+			this.riga = riga;
+			this.stringa = stringa;
+		}
+	}
 	public abstract Automa getAutoma();
 	public void main(){
 
         Automa automa = this.getAutoma();
-        
-        Scanner sc = new Scanner(System.in);
-        boolean allOk = true;
-        
         String s;
-        while(sc.hasNext()){
+        Scanner sc = new Scanner(System.in);
+        List<ErrorBox> errorList = new LinkedList<ErrorBox>();
+    	boolean check;
+        for(int i = 0; sc.hasNext(); i++){
+        	check = true;
         	s = sc.next();
-        	System.out.print("Stringa: " + s + " Verificata: ");
-            System.out.println(automa.match(Arrays.asList(s.split(""))));
+        	System.out.print(i + " Stringa: " + s + " Verificata: ");
+        	if(!(check = automa.match(Arrays.asList(s.split("")))))
+        		errorList.add(new ErrorBox(i, s));
+        			
+            System.out.println(check);
         }
         sc.close();
+        
+        if(!errorList.isEmpty()){
+        	System.out.println("Numero errori: " + errorList.size());
+        	int i = 0;
+        	for(ErrorBox er : errorList)
+        		System.out.println((++i)+" Riga: "+ er.riga +" Stringa: " + er.stringa);
+        }
 	}
 }
