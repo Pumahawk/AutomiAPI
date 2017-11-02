c86c526089db83bb82c8e83695b6c4474ef4285a
Lorenzo Gandino <lorenzoga94@gmail.com>
2017-11-02 21:47:14 +0100

    Aggiunto esercizio 2.3

---
+++ src/lexer/Es2_3.java
@@ -0,0 +1,38 @@
+package lexer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Es2_3 {
+	public static void main(String[] args) throws FileNotFoundException {
+    	boolean status = true;
+    	//BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedReader in = new BufferedReader(new FileReader(new File("input-file/esp2_3")));
+    	
+    	UniLexer lexer = new UniLexer(in);
+    	Token c = new Token(0);
+    	while(c.tag != Tag.EOF) {
+    		try {
+    			c = lexer.nextToken();
+    			if((c == null)) {
+    				status = false;
+    				break;
+    			}
+				System.out.println(c);
+    			if((c.tag == Tag.EOF))
+    				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    	if(status) {
+        	System.out.println("Terminazione corretta");
+    	}
+    	else
+
+        	System.out.println("Terminazione errata");
+    }
+}
